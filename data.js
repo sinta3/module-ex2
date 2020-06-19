1f0237fb78cea8cca8300ed613796982471229fd
sinta3 <sinta.tri38@gmail.com>
2020-06-19 15:04:21 +0700

    add all file

---
+++ data.js
@@ -0,0 +1,20 @@
+let a = 4;
+let b = 3;
+
+function plus() {
+    return a + b;
+}
+
+function times() {
+    return a * b;
+}
+
+function minus() {
+    return a - b;
+}
+
+function divide() {
+    return a / b;
+}
+
+export { plus, times, minus, divide };
---
+++ script.js
@@ -0,0 +1,4 @@
+import { plus, minus, times, divide } from "./data.js";
+console.log(
+    ` hasil penjumlahan : ${plus()} \n hasil pengurangan : ${minus()} \n hasil perkalian : ${times()} \n hasil pembagian : ${divide()}`
+);
